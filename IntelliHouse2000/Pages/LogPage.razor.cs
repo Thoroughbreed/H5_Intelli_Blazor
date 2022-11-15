1e1ee9ea33be8031b55bd447b631f78c2da4805e
Peter Hymøller <peterhym21@gmail.com>
2022-11-15 09:43:32 +0100

    Pages Updated

---
+++ IntelliHouse2000/Pages/LogPage.razor.cs
@@ -0,0 +1,36 @@
+using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using Toolbelt.Blazor.HotKeys;
+
+namespace IntelliHouse2000.Pages
+{
+    public partial class LogPage
+    {
+
+        [Inject]
+        private IToastService ToastService { get; set; }
+        [Inject]
+        private HotKeys HotKeys { get; set; }
+        [Inject]
+        private Toolbelt.Blazor.I18nText.I18nText I18nText { get; set; }
+
+
+        HotKeysContext? HotKeysContext;
+        I18nText.LanguageTable languageTable = new I18nText.LanguageTable();
+
+        protected override async Task OnInitializedAsync()
+        {
+            languageTable = await I18nText.GetTextTableAsync<I18nText.LanguageTable>(this);
+
+            this.HotKeysContext = this.HotKeys.CreateContext()
+                .Add(ModKeys.None, Keys.F8, Toaster);
+        }
+
+        void Toaster() => ToastService.ShowInfo("Congtats ypu just used a Hotkey: F8", "HotKey");
+
+        public void Dispose() => HotKeysContext.Dispose();
+
+
+    }
+}
