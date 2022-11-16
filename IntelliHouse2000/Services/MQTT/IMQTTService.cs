b8c693db6534e88839e61d1ea7c826fc59ef6d2b
Nicolai Brock Heuck <nico061c@elevcampus.dk>
2022-11-16 13:25:39 +0100

    Added unsubscribe to MQTT service

--- IntelliHouse2000/Services/MQTT/IMQTTService.cs
+++ IntelliHouse2000/Services/MQTT/IMQTTService.cs
@@ -1,5 +1,7 @@
 ﻿using IntelliHouse2000.Helpers;
 using MQTTnet;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
 using MQTTnet.Client.Options;
 using MQTTnet.Protocol;
 
@@ -8,6 +10,8 @@ namespace IntelliHouse2000.Services.MQTT;
 public interface IMQTTService
 {
     public Task<bool> Subscribe(string topic);
+    public Task<bool> Unsubscribe(string topic);
+    
     public void Initialize(IMqttClientOptions mqttClientOptions);
     public Task<bool> Connect();
     public bool IsConnected();
@@ -15,7 +19,11 @@ public interface IMQTTService
     public Task<bool> Reconnect();
     public Task StartAsync();
     
-    Task<bool> Publish(MqttApplicationMessage message);
-    Task<bool> Publish(string topic, byte[] payload, bool retain = false, MqttQualityOfServiceLevel qos = MqttQualityOfServiceLevel.AtMostOnce);
-    Task<bool> Publish(string topic, string payload, bool retain = false, MqttQualityOfServiceLevel qos = MqttQualityOfServiceLevel.AtMostOnce);
+    public Task<bool> Publish(MqttApplicationMessage message);
+    public Task<bool> Publish(string topic, byte[] payload, bool retain = false, MqttQualityOfServiceLevel qos = MqttQualityOfServiceLevel.AtMostOnce);
+    public Task<bool> Publish(string topic, string payload, bool retain = false, MqttQualityOfServiceLevel qos = MqttQualityOfServiceLevel.AtMostOnce);
+    
+    public event EventHandler<MqttClientConnectedEventArgs> Connected;
+    public event EventHandler<MqttClientDisconnectedEventArgs> Disconnected;
+    public event EventHandler<MqttApplicationMessageReceivedEventArgs> MessageReceived;
 }
