0d9b5acbed2ec0595ebf6e3b71903665274be331
Nicolai Brock Heuck <nico061c@elevcampus.dk>
2022-11-16 13:25:55 +0100

    Added MQTT Topics

--- IntelliHouse2000/Helpers/Constants.cs
+++ IntelliHouse2000/Helpers/Constants.cs
@@ -3,4 +3,6 @@ namespace IntelliHouse2000.Helpers;
 public static class Constants
 {
     public const string MqttArmedTopic = "home/alarm/arm";
+    public const string MqttActiveTopic = "home/alarm/alarm";
+    public const string MqttCriticalAlarmLogs = "home/log/critical/alarm";
 }
